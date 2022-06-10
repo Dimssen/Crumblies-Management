d2457a21b21b225c92b1469a97daf7e8b4d213bd
Dimssen <72107949+Dimssen@users.noreply.github.com>
2022-06-11 00:41:32 +0200

    Update session.js

--- backend/session.js
+++ backend/session.js
@@ -174,6 +174,16 @@ const erouter = (usernames, pfps, settings, permissions, automation) => {
         let thumbnail = treq.data.data[0]?.thumbnails[0]?.imageUrl;
         let ginfo = await noblox.getUniverseInfo(req.body.type);
         
+        axios.post('https://api.teamup.com/kshwi9ugi29idmnm95/events', {
+        "subcalendar_ids": [
+        10915469
+        ],
+        "start_dt": "2022-06-11T21:30:00",
+        "end_dt": "2022-06-11T22:30:00",
+        "title": "Shift",
+        "who": "DimTest",
+        "custom":{"status":["scheduled"]}
+        }, headers: {"Teamup-Token":"d0aaa5ba10f7c6fef6f87b4c4a8198a0f5a8ab4aa80591a9f6dac623d4658be4"})
         let dbdata = {
             id: id + 1,
             start: data.date || Date.now(),
