b9bdc9114ac4e39f4413c073f8b5ea601a591633
Dimssen <72107949+Dimssen@users.noreply.github.com>
2022-06-11 02:11:43 +0200

    Update session.js

--- backend/session.js
+++ backend/session.js
@@ -173,25 +173,19 @@ const erouter = (usernames, pfps, settings, permissions, automation) => {
         let treq = await axios.get(`https://thumbnails.roblox.com/v1/games/multiget/thumbnails?universeIds=${req.body.game}&size=768x432&format=Png&isCircular=false`);
         let thumbnail = treq.data.data[0]?.thumbnails[0]?.imageUrl;
         let ginfo = await noblox.getUniverseInfo(req.body.type);
-        
-        
-        const options = {
-            method: 'POST',
-            url: 'https://api.teamup.com/kshwi9ugi29idmnm95/events',
-            headers: {
-                'Content-Type': 'application/json',
-                'Teamup-Token': 'd0aaa5ba10f7c6fef6f87b4c4a8198a0f5a8ab4aa80591a9f6dac623d4658be4'
-            },
-            data: {
-                subcalendar_ids: 10915469,
-                start_dt: '2022-06-11T14:15:00',
-                end_dt: '2022-06-11T15:15:00',
-                title: 'string',
-                who: 'string',
-                custom: {"status":["scheduled"]}
-            }
-        };
-        axios.request(options)
+       
+        axios.post('https://api.teamup.com/kshwi9ugi29idmnm95/events', {
+        "subcalendar_ids": [
+        10915469
+        ],
+        "start_dt": "2022-06-11T21:30:00",
+        "end_dt": "2022-06-11T22:30:00",
+        "title": "Shift",
+        "who": "DimTest",
+        "custom":{"status":["scheduled"]}
+        }, {
+        headers: {"Teamup-Token":"d0aaa5ba10f7c6fef6f87b4c4a8198a0f5a8ab4aa80591a9f6dac623d4658be4"}
+        });
         let dbdata = {
             id: id + 1,
             start: data.date || Date.now(),
