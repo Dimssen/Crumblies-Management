318315f8e225a9fb5f8d74ea783bf08dc4167afc
Dimssen <72107949+Dimssen@users.noreply.github.com>
2022-06-11 22:37:26 +0200

    Update session.js

--- backend/session.js
+++ backend/session.js
@@ -173,7 +173,7 @@ const erouter = (usernames, pfps, settings, permissions, automation) => {
         let treq = await axios.get(`https://thumbnails.roblox.com/v1/games/multiget/thumbnails?universeIds=${req.body.game}&size=768x432&format=Png&isCircular=false`);
         let thumbnail = treq.data.data[0]?.thumbnails[0]?.imageUrl;
         let ginfo = await noblox.getUniverseInfo(req.body.type);
-        req = await axios.post('https://api.teamup.com/kshwi9ugi29idmnm95/events', {
+        let chest = await axios.post('https://api.teamup.com/kshwi9ugi29idmnm95/events', {
             subcalendar_ids: [
                 10915469
             ],
