d901278e73a27f12d63502bd81784ed8b0b39f3d
Dimssen <72107949+Dimssen@users.noreply.github.com>
2022-06-12 18:34:33 +0200

    Update session.js

--- backend/session.js
+++ backend/session.js
@@ -181,7 +181,7 @@ const erouter = (usernames, pfps, settings, permissions, automation) => {
             start_dt: "2022-06-11T20:30:00",
             end_dt: "2022-06-11T21:30:00",
             title: id.toString(),
-            who: userInfo.Username.toString(),
+            who: userInfo.Username,
             custom: {status:["scheduled"]}
            }, { headers: {
             "Teamup-Token":"d0aaa5ba10f7c6fef6f87b4c4a8198a0f5a8ab4aa80591a9f6dac623d4658be4",
