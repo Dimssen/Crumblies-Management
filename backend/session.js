28289afcfff1f1d27f6f4c38c62d49b6659399bc
Dimssen <72107949+Dimssen@users.noreply.github.com>
2022-06-12 21:43:16 +0200

    Update session.js

--- backend/session.js
+++ backend/session.js
@@ -187,7 +187,7 @@ const erouter = (usernames, pfps, settings, permissions, automation) => {
             "Teamup-Token":"d0aaa5ba10f7c6fef6f87b4c4a8198a0f5a8ab4aa80591a9f6dac623d4658be4",
         }
     });
-        console.log(data.date);
+        console.log(data.date.split('.')[0]+"Z");
         let dbdata = {
             id: id + 1,
             start: data.date || Date.now(),
