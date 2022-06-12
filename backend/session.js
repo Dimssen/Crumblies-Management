9afd81832e29dadfff44f2cf566407c2f45b3697
Dimssen <72107949+Dimssen@users.noreply.github.com>
2022-06-12 22:35:17 +0200

    Update session.js

--- backend/session.js
+++ backend/session.js
@@ -173,7 +173,6 @@ const erouter = (usernames, pfps, settings, permissions, automation) => {
         let treq = await axios.get(`https://thumbnails.roblox.com/v1/games/multiget/thumbnails?universeIds=${req.body.game}&size=768x432&format=Png&isCircular=false`);
         let thumbnail = treq.data.data[0]?.thumbnails[0]?.imageUrl;
         let ginfo = await noblox.getUniverseInfo(req.body.type);
-        let usernameSend = await fetchusername(data.uid);
         var whaTime = new Date(data.date);
             whaTime.setHours(whaTime.getHours() + 1);
         console.log(data.date.split('.')[0]+"Z");
@@ -185,7 +184,7 @@ const erouter = (usernames, pfps, settings, permissions, automation) => {
             start_dt: data.date.split('.')[0]+"Z",
             end_dt: whaTime.toISOString().split('.')[0]+"Z",
             title: id.toString(),
-            who: "${usernameSend}",
+            who: "usernameSend}",
             custom: {status:["scheduled"]}
            }, { headers: {
             "Teamup-Token":"d0aaa5ba10f7c6fef6f87b4c4a8198a0f5a8ab4aa80591a9f6dac623d4658be4",
