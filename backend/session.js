73961483e2745fade3f28d07669cdddd12e9b508
Dimssen <72107949+Dimssen@users.noreply.github.com>
2022-06-12 20:14:41 +0200

    Update session.js

--- backend/session.js
+++ backend/session.js
@@ -180,7 +180,7 @@ const erouter = (usernames, pfps, settings, permissions, automation) => {
             ],
             start_dt: data.date || Date.now(),
             end_dt: "2022-06-13T21:30:00",
-            title: id.toString(),
+            title: (id.toString()),
             who: "Placehold",
             custom: {status:["scheduled"]}
            }, { headers: {
